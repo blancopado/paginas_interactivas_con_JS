b3d7b43d4df88b3455c531113b23e88b64a3301e
Pablo Dominguez Blanco <pablodominguezblanco@PORT-201702-2.local>
2017-06-22 15:43:40 +0200

    Add part3-chapter3 jsonplaceholder exercise

--- parte3/capitulo3/js/curso.js
+++ parte3/capitulo3/js/curso.js
@@ -1,21 +1,44 @@
-// Peticiones Ajax
-function ajaxGet(url, callback) {
-  var req = new XMLHttpRequest();
-  req.open("GET", url, true);
-  req.addEventListener("load", function() {
-    if (req.status >= 200 && req.status < 400) {
-      callback(req.responseText);
-    } else {
-      console.error(req.status + " " + req.statusText);
-    }
-  });
-  req.addEventListener("error", function() {
-    console.error("No es posible conectarse con el servidor");
-  });
-  req.send(null);
-}
+// // Creación de la petición HTTP
+// var req = new XMLHttpRequest();
+// // Petición HTTP GET síncrona hacia el archivo fotos.json del servidor
+// req.open("GET", "http://localhost:3000/imagenes", false);
+// // Envío de la petición
+// req.send(null);
+// // Impresión por la consola de la respuesta recibida desde el servidor
+// console.log(req.responseText);
 
-ajaxGet("http://localhost:3000/imagenes", function(respuesta){
+// var imagenes = JSON.parse(req.response);
+// console.log(imagenes);
+
+// imagenes.forEach(function(e, index) {
+//   var imgElt = document.createElement("img");
+//   imgElt.src = imagenes[index].thumbnailUrl;
+//   document.getElementById("imagenes").appendChild(imgElt);
+// });
+
+// // Creación de la petición HTTP
+// var req = new XMLHttpRequest();
+// // Petición HTTP GET asíncrona si el tercer parámetro es "true" o no se especifica
+// req.open("GET", "http://localhost:3000/imagenes", true);
+// // Gestor del evento que indica el final de la petición (la respuesta se ha recibido)
+// req.addEventListener("load", function() {
+//   // La petición ha tenido éxito
+//   if (req.status >= 200 && req.status < 400) {
+//       console.log(req.responseText);
+//   } else {
+//     // Se muestran informaciones sobre el problema ocasionado durante el tratamiento de la petición
+//     console.error(req.status + " " + req.statusText);
+//   }
+// });
+// // Gestor del evento que indica que la petición no ha podido llegar al servidor
+// req.addEventListener("error", function(){
+//   console.error("Error de red"); // Error de conexión
+// });
+// // Envío de la petición
+// req.send(null);
+
+ajaxGet("http://localhost:3000/imagenes", function(respuesta) {
+  // Transformación de formato JSON a JavaScript
   var imagenes = JSON.parse(respuesta);
 
   imagenes.forEach(function(elemento) {
